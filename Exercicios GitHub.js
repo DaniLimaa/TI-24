b091af63027b8d1a1febc7227a0670b46eb98b69
Daniel Camargo <169094692+DaniLimaa@users.noreply.github.com>
2024-06-22 13:42:19 -0300

    Exercicios GitHub.js

--- Exercicios GitHub.js
+++ Exercicios GitHub.js
@@ -189,3 +189,37 @@ else if (mes == 'fevereiro'){
 console.log()
 
 //12- Faça um programa que verifique se um trianguço é equilatero, isoceles ou escaleno com base nos comprimentos de seus lados
+
+medida1 = 9
+medida2 = 8
+medida3 = 3
+
+if (medida1 == medida2 == medida3){
+    console.log('O triangulo é equilatero')
+}
+else if (medida3 = medida2 == medida1){
+    console.log('O triangulo é equilatero')
+}
+else if (medida2 == medida3 == medida1){
+    console.log('O triangulo é equilatero')
+}
+// muita coisa
+else if (medida1 == medida2 != medida3){
+    console.log('O triangulo é isoceles')
+}
+else if (medida1 != medida2 == medida3){
+    console.log('O triangulo é isoceles')
+}
+else if (medida2 != medida1 == medida3){
+    console.log('O triangulo é isoceles')
+}
+// muita coisa
+else if (medida1 != medida2 != medida3){
+    console.log('O triangulo é escaleno')
+}
+else if (medida3 != medida2 != medida1){
+    console.log('O triangulo é escaleno')
+}
+else if (medida2 != medida3 != medida1){
+    console.log('O triangulo é escaleno')
+}
